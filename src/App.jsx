f1b962235b7e62110f4d7b47bf0efbd04f1401ee
Yorman Eduardo Amaro Carrero <yormaneduardoamarocarrero@yormans-mbp.lan>
2023-01-23 21:44:21 -0300

    updating the structure of folders, files and uploading images of the project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Pilots from "./components/Pilots/Pilots";
+import "./global.css";
+
+function App() {
+  return (
+    <div>
+      hello i am App para este proyecto
+      <Pilots/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Pilots/Pilots.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import stylePilots from "./style.module.css"
+
+
+
+const Pilots = () => {
+    return (
+      <div className={stylePilots.containerPilots}>
+        <img 
+          className='stylePilots.imgPilots' 
+          src="/" alt="" />
+      </div>
+
+    )
+}
+
+export default Pilots
+
+
+
