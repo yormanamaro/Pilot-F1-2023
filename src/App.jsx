4a9507e78e426ac0d59b25dc2d3f84a6829e4bff
Yorman Eduardo Amaro Carrero <yormaneduardoamarocarrero@yormans-mbp.lan>
2023-01-26 18:20:12 -0300

    Creating pilots component and applying general styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import React from "react";
-import Pilots from "./components/Pilots/Pilots";
-import "./global.css";
+import React from 'react'
+import Pilots from './components/Pilots/Pilots';
+import './global.css';
 
 function App() {
   return (
-    <div>
-      hello i am App para este proyecto
-      <Pilots/>
+    <div className='App'>
+      <h1 className='title'>My list of favorite formula one drivers</h1>
+      <Pilots />
     </div>
   );
-}
+};
 
 export default App;
+
+
+
+// QUEDAMOS EN EL MINUTO 2:09:20 (INSERTANDO ESTILOS AL CONTENEDOS PADRE APP)
+
+
--- src/components/Pilots/Pilots.jsx
+++ src/components/Pilots/Pilots.jsx
@@ -1,20 +1,32 @@
 import React from 'react'
-import stylePilots from "./style.module.css"
+import stylePilots from './style.module.css'
 
 
 
-const Pilots = () => {
+const Pilots = () => { // Saber que require es una de las formas de insertar una img 
     return (
       <div className={stylePilots.containerPilots}>
         <img 
           className='stylePilots.imgPilots' 
-          src="/" alt="" />
+          src={require('../images/piloto-fernandoAlonso.png')} 
+          alt="Photography by Fernando Alonso" /> 
+          <div className='stylePilots.container_information'>
+            <p className='stylePilots.pilot_name'>Fernando Alonso from Spain</p>
+            <p className='stylePilots.team_pilot'>Driver of the Aston Martin Aramco Cognizant Formula One Team</p>
+            <p className='stylePilots.pilot_story'>"Fiercely competitive, Alonso is not shy about his talent, rating himself as 9/10 “in everything”, and few in the know would disagree, with his performances in F1 characterised by blistering speed, brilliant tactical thinking, exemplary race craft, a razor-sharp eye for detail and a relentless determination to win."</p>
+          </div>
       </div>
 
-    )
-}
+    );
+};
 
 export default Pilots
+
+
+
+
+
+
 
 
 
